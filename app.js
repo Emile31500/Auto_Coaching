86b80b21dc7c3b1dc35a830cf4207c002ba38bfc
Emile <emile00013@gmail.com>
2023-10-18 18:05:41 +0200

    resolve merge conflict with auth

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const session = require('express-session');
 const dotenv = require('dotenv').config();
 
 const app = express();
-<<<<<<< Updated upstream
-=======
 const jwt = require('jsonwebtoken')
 const bodyParser = require('body-parser');
 var router = express.Router();
->>>>>>> Stashed changes
 
 const { sequelize } = require('./models'); 
 const { User } = require('./models');
