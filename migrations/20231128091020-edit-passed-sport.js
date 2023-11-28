9cf0e3483a193057907306bc552394487d351b02
Emile <emile00013@gmail.com>
2023-11-28 12:23:03 +0100

    GET / API.POST Request Train && idResquestTrain=null ?

--- migrations/20231128091020-edit-passed-sport.js
+++ migrations/20231128091020-edit-passed-sport.js
@@ -3,17 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    
     return Promise.all([
-      queryInterface.addConstraint('Measurments', {
-        fields: ['userId'],
-        type: 'foreign key',
-        name: 'measurment_user_assoc',
-        references: {
-          table: 'Users',
-          field: 'id'
-        }
-      })
+      
+      queryInterface.addColumn('passedsports', 'startDate', Sequelize.DATE ),
+      queryInterface.addColumn('passedsports', 'endDate', Sequelize.DATE )
+    
     ]);
+
   },
 
   async down (queryInterface, Sequelize) {
--- routes/train.js
+++ routes/train.js
@@ -1,11 +1,56 @@
 const express = require('express');
+const {TrainRequest, PassedSport, User} = require('../models/');
+
+var authenticationChecker = require('../middlewares/authenticationChecker')
 
 const router = express.Router()
 
-router.get('/train', (req, res) => {
+router.get('/train', authenticationChecker, (req, res) => {
 
-    res.render('../views/train',  { food: food, layout: '../views/main' });
+    res.render('../views/train',  {layout: '../views/main' });
 
+
+})
+
+router.get('/train/request', authenticationChecker, (req, res) => {
+
+    res.render('../views/train-request',  {layout: '../views/main' });
+
+
+})
+
+router.post('/api/train/request', authenticationChecker, async (req, res) => {
+
+    var user = await User.findOne({where: {authToken:  req.session.token}});
+    const userId = user.id;
+    var passedSports = []; 
+
+    const trainRequest = TrainRequest.create({userId: userId}, req.body);
+
+    if (req.body.passedSport.length > 0){
+
+        var index = 0;
+        req.body.passedSport.forEach(row => {
+
+            let tempPS = PassedSport.create(JSON.parse(row));
+            passedSports.push(tempPS);
+            index++;
+
+        });
+    }
+
+    if (trainRequest) {
+
+        res.statusCode = 201
+        
+        res.send({'code': 201,'message': 'Train request has been created' ,'data': req.body});
+
+    } else {
+
+        res.statusCode = 404;
+        res.send({'message' : 'This food was not found of id ' + id});
+
+    }
 
 })
 
