e813649205855c94c17753d17e7d7d50dbe73c02
Emile <emile00013@gmail.com>
2024-03-01 10:03:11 +0100

    GET message && GET Api message && POST Api message : just for simple user, not admin

---
+++ models/message.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Message extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+
+      Message.belongsTo(models.User, { foreignKey: 'userId', targetKey: 'id', as: 'sender' });
+      Message.belongsTo(models.User, { foreignKey: 'userIdRecipient', targetKey: 'id', as : 'recipient' });
+
+    }
+  }
+  Message.init({
+    userId: DataTypes.INTEGER,
+    isDelated: DataTypes.BOOLEAN,
+    isViewed: DataTypes.BOOLEAN,
+    message: DataTypes.STRING,
+    userIdRecipient: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Message',
+    tableName: 'messages',
+
+  });
+  return Message;
+};
---
+++ public/js/message/send.js
@@ -0,0 +1,35 @@
+sendButton.addEventListener('click', async function (event){
+
+    event.preventDefault();
+    message = messageInput.value;
+    const todayDateTime =  getTodayDateTime()
+
+    const rawData = JSON.stringify({
+        userId : null,
+        message : message,
+        isViewed : false,
+        isDelated : false,
+        createdAt : todayDateTime,
+        updatedAt : todayDateTime,
+        userIdRecipient : null
+    });
+
+    const res = await fetch('/api/message', {
+        method : 'POST',
+        headers : {
+            'Content-type' : 'application/json'
+        },
+        body : rawData
+    }).then(response => {return response.json()})
+    .then(data => {
+        return data;
+    })
+    
+    wrightMessage()
+    if (201 !== res.code){
+
+        alert('error')    
+
+    }
+
+})
