0e77093faf10f8c4416951f50109cb393a8adf84
Emile <emile00013@gmail.com>
2024-03-04 14:27:47 +0100

    GET Message, GET Api message && POST Api message for user and admin

--- public/js/date.js
+++ public/js/date.js
@@ -1,7 +1,7 @@
 function getTodayDate(date = undefined) {
 
     let date_ob;
-
+    let monthAdditioner = 0
     if (date){
 
         date_ob = new Date(date);
@@ -9,35 +9,50 @@ function getTodayDate(date = undefined) {
     } else {
 
         date_ob = new Date();
+        monthAdditioner = 1
 
     }
 
-    let day = ("0" + date_ob.getDate()).slice(-2);
-    let month = ("0" + (date_ob.getMonth()+1)).slice(-2);
+    let day = date_ob.getDate();
+    let month = date_ob.getMonth()+monthAdditioner;
+
+    if (month < 10) {
+
+        month = "0" + month
+    }
+    if (day < 10) {
+
+        day = "0" + day
+    }
+
     let year = date_ob.getFullYear();
 
-    return (year + "-" + month + "-" + day + ' 00:00:00');
+    return (year + "-" + month + "-" + day);
 
 }
-
 
 function getTodayDateTime(date = undefined) {
     
     let date_ob;
+    let monthAdditioner = 0
+    let hoursAdditioner = 0
 
     if (date){
         date_ob = new Date(date);
     } else {
         date_ob = new Date();
+        monthAdditioner = 1
+        hoursAdditioner = 1
+
     }
 
-    let day = ("0" + date_ob.getDate()).slice(-2);
-    let month = ("0" + (date_ob.getMonth()+1)).slice(-2);
+    let day = date_ob.getDate();
+    let month = date_ob.getMonth()+monthAdditioner;
     let year = date_ob.getFullYear();
 
-    let hours = ("0" + date_ob.getHours()).slice(-2);
-    let minutes = ("0" + date_ob.getMinutes()).slice(-2);
-    let seconds = ("0" + date_ob.getSeconds()).slice(-2);
+    let hours = date_ob.getHours()+hoursAdditioner;
+    let minutes = date_ob.getMinutes();
+    let seconds = date_ob.getSeconds();
 
     return (year + "-" + month + "-" + day + ' ' + hours + ':' + minutes + ':' + seconds);
 }
