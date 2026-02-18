8de9535cf4ee789841130c9d513636d929c4fa38
Emile31500 <emile00013@gmail.com>
2026-02-18 17:54:55 +0100

    test to creat a purschasing

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,35 +1,63 @@
- const stripe = Stripe("pk_test_51JSe6xJEkapOWG0uMvUjdASEMRmuay3yhmeLPLLazRBxhR6WqlAXF1GrpzfFSQzBMMwSHJZffxxcYFv9njK2jnNo00lRkAazE4"); // clé publique
-  const elements = stripe.elements();
+const stripe = Stripe("pk_test_51SxQASJ7uGufFKhIp92vPaUU3Fx4wN2RsuSYr639oSIcw8PXbsFfhAssEAnegVDBYmommSyQk8RKTv0KGzGoegcW00BuzIUHwH"); // clé publique
+const elements = stripe.elements();
 
-  const card = elements.create("card");
-  card.mount("#card-element");
+const emailInput = document.querySelector('#email')
+const card = elements.create("card");
+card.mount("#card-element");
 
-  const form = document.getElementById("payment-form");
 
-  form.addEventListener("submit", async (event) => {
+const form = document.getElementById("payment-form");
+const formDataUser = document.querySelector('.formDataUser');
+
+form.addEventListener("submit", async (event) => {
     event.preventDefault();
+    const formData = new FormData(formDataUser);
+    const email = emailInput.value;
+    
+    const { paymentMethod, errorPaymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: card,
+      billing_details: {
+        email: email,
+      },
+    });
+
+    
+    
+    formData.append("paymentMethodId", paymentMethod.id);    
+    const formDataObject = Object.fromEntries(formData.entries());
+    const formDataString = JSON.stringify(formDataObject);
+
 
     // Appel backend pour créer le PaymentIntent
     const response = await fetch("/create-payment-intent", {
       method: "POST",
-      headers: { "Content-Type": "application/json" }
+      headers: { "Content-Type": "application/json" },
+      body : formDataString
     });
 
     const data = await response.json();
     const clientSecret = data.clientSecret;
 
-    const { error, paymentIntent } = await stripe.confirmCardPayment(
+    await stripe.confirmCardSetup(clientSecret, {
+      payment_method: {
+        card: card,
+        billing_details: { email : email }
+      }
+    });
+
+    /*const { error, paymentIntent } = await stripe.confirmCardPayment(
       clientSecret,
       {
         payment_method: {
           card: card
         }
       }
-    );
+    );/**/
 
     if (error) {
       document.getElementById("error-message").textContent = error.message;
     } else if (paymentIntent.status === "succeeded") {
       alert("Paiement réussi 🎉");
     }
-  });
+});
--- routes/home.js
+++ routes/home.js
@@ -17,20 +17,71 @@ router.get('/cgv', isAuth, async (req, res) => {
     });
 });
 
+router.post("/create-subscription", async (req, res) => {
+  try {
+    const { email, paymentMethodId } = req.body;
 
+    console.log(paymentMethodId)
+
+    // 1️⃣ Créer le client
+    const customer = await stripe.customers.create({
+      email: email,
+      payment_method: paymentMethodId,
+      invoice_settings: {
+        default_payment_method: paymentMethodId,
+      },
+    });
+
+    // 2️⃣ Créer l'abonnement
+    const subscription = await stripe.subscriptions.create({
+        customer: customer.id,
+        items: [{ price: 'price_1T1p2WJ7uGufFKhI58gwnOec'},],
+        /*payment_behavior: 'default_incomplete',
+        payment_settings: {
+            save_default_payment_method: "on_subscription",
+        },*/
+        expand: ['latest_invoice.payment_intent'],
+    });
+
+    res.json({
+      subscriptionId: subscription.id,
+      clientSecret:
+        subscription.latest_invoice.payment_intent.client_secret,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+/*
 router.post("/create-payment-intent", parserJson, async (req, res) => {
 
     try {
 
         const rawData = req.body;
-        console.log(rawData)
 
-        if (rawData.password === rawData.passwordConf) {
+        const customer = await stripe.customers.create({
+            email: rawData.email,
+        });
+
+
+        const setupIntent = await stripe.setupIntents.create({
+            customer: customer.id,
+        });
+
+         res.status(200).json({
+            // subscriptionId: subscription.id,
+            clientSecret: setupIntent.client_secret
+                // subscription.latest_invoice.payment_intent.client_secret,
+        });/**/
+
+
+        /*if (rawData.password === rawData.passwordConf) {
         
             const isUser = await User.findOne({where : { email : rawData.email}})
-            console.log(isUser)
 
-        
             if (!(isUser instanceof User)){
                 
                 const hashedPassword = pbkdf2.hashSync(rawData.password);
@@ -44,7 +95,6 @@ router.post("/create-payment-intent", parserJson, async (req, res) => {
                     role: ["user"]
                 });
                 await user.save()
-                console.log(user)
 
                 const nutritionRequirement = await NutritionRequirement.create({
                     personnalMultiplicator : 1.0,
@@ -53,7 +103,6 @@ router.post("/create-payment-intent", parserJson, async (req, res) => {
                     fatMultiplicator : 1.0,
                     userId : user.id
                 })
-                console.log(nutritionRequirement instanceof NutritionRequirement)
 
                 const customer = await stripe.customers.create({
                     email: rawData.email,
@@ -62,7 +111,6 @@ router.post("/create-payment-intent", parserJson, async (req, res) => {
                         default_payment_method: rawData.paymentMethodId,
                     },
                 });
-                console.log(customer)
 
                 const now = new Date();
                 const endDate = now;
@@ -72,20 +120,21 @@ router.post("/create-payment-intent", parserJson, async (req, res) => {
                     customer: customer.id,
                     items: [
                         {
-                            price: rawData.priceId, 
+                            price: rawData.priceId,
                         },
                     ],
-                    ended_at : endDate.getTime(),
-                    // status: "active",
+                    cancel_at: Math.floor(endDate.getTime() / 1000),
                     payment_behavior: "default_incomplete",
                     expand: ["latest_invoice.payment_intent"],
-                });/**/
+                });
+                console.log(subscription)
 
-                const authToken = await user.getAuthenticationToken();
-                req.session.token = authToken;
+                res.status(200).json({
+                    subscriptionId: subscription.id,
+                    clientSecret:
+                        subscription.latest_invoice.payment_intent.client_secret,
+                });
 
-                req.flash('success', 'Merci infiniment de nous avoir choisit ! Nous espérons que nous serons à la hauteur de vos attentes ! ')
-                res.redirect('/profile')
 
             } else throw 'Un utilisateur avec cette adresse maile existe déjà';
 
@@ -94,14 +143,49 @@ router.post("/create-payment-intent", parserJson, async (req, res) => {
 
     } catch (error) {
 
-        req.flash('danger', error);
-        res.redirect('/')
+        console.error(error);
+        res.status(400).json({ error: error.message });
 
     }
      
 
-});
+});*/
 
+
+router.get('/', isAuth, async (req, res) => {
+
+
+    if (req.user instanceof User) {
+        res.redirect('/profile')
+    } else {
+        
+        users = await User.findAll()
+        for (let index = 0; index < users.length; index++) {
+
+            if (users[index].role.includes('admin')) users.splice(index, 1);
+            
+        }
+
+        const numberOfPLace = process.env.NUMBER_OF_PLACE;
+        const count = users.length
+        
+        if (numberOfPLace > 100) {
+            disposablePlace = Math.ceil((numberOfPLace - count)/50)*50;
+        } else {
+            disposablePlace = (numberOfPLace - count);
+        }
+
+        res.locals.message = req.flash()
+        res.render('../views/index',  { 
+            disposablePlace : disposablePlace,
+            page : '/',
+            user : req.user,
+            layout : '../views/main' 
+        });
+    }
+
+})
+/*
 router.get('/', isAuth, async (req, res) => {
 
 
@@ -134,6 +218,6 @@ router.get('/', isAuth, async (req, res) => {
         });
     }
 
-})
+})*/
 
  module.exports = router;
