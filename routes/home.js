f6266cf32fb5eb4b3c02342a091f68f4eeab4106
Emile31500 <emile00013@gmail.com>
2026-02-20 19:07:31 +0100

    contact and mail on subscription

--- routes/home.js
+++ routes/home.js
@@ -8,9 +8,101 @@ const isAuth = require('../middlewares/isAuth');
 const parserJson = require('../middlewares/parserJson');
 const pbkdf2 = require("hash-password-pbkdf2")
 
+const nodemailer = require("nodemailer");
+
+// Create a transporter using Ethereal test credentials.
+// For production, replace with your actual SMTP server details.
+/*const transporter = nodemailer.createTransport({
+  service: "gmail", // Shortcut for Gmail's SMTP settings - see Well-Known Services
+  auth: {
+    type: "OAuth2",
+    user: "emile00013@gmail.com",
+    // clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret:  "edsryxgncragwpfc",
+    // refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+  },
+})*/
+
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.SMTP_FROM,
+    pass: process.env.SMTP_PASS, 
+  },
+});
+
+// gmail+smtp://:edsryxgncragwpfc@default*/
+
+
+  
+
+
+router.get('/bibliography', async (req, res) => {
+    res.render('../views/home/bibliography',  { 
+        user : req.user, 
+        page : '/bibliography',
+        layout: '../views/main' 
+    });
+})
+
+router.get('/contact', async (req, res) => {
+
+    res.locals.message = req.flash();
+    res.render('../views/home/contact',  { 
+        user : req.user, 
+        page : '/contact',
+        layout: '../views/main' 
+    });
+})
+
+router.post('/contact', parserJson, async (req, res) => {
+
+
+    try {
+
+        const rawData = req.body
+
+        await transporter.sendMail({
+            from: rawData.email,
+            to: process.env.SMTP_FROM,
+            subject: rawData.subject,
+            html: `<p>${rawData.content}</p> <p>Répondre à <a href='mailto:${rawData.email}'>${rawData.firstname}</a></p>`, // Plain-text version of the message
+        });
+
+        const today = new Date();
+
+            await transporter.sendMail({
+                from: process.env.SMTP_FROM,
+                to: rawData.email,
+                subject: rawData.objet,
+                html: `<p>Bonjour ${rawData.firstname} ${rawData.lastname},</p><p> Votre message ayant pour objet ${rawData.objet} envoyé le ${today.toLocaleDateString('fr-FR', {
+                    weekday: 'long',
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric'
+                }) } a bien été reçu par l'équipe de regimesenior.fr<br>Nous le traiterons dans les plus brefs délais.</p><p>Cordialement</p><p>L'équipe regimesenior</p>`
+            });
+
+
+
+        req.flash('success', "Votre message a bien été envoyé ! Surveillez l'adresse " )
+
+
+    } catch(error) {
+
+        req.flash('danger', error.message)
+
+    }
+
+    res.redirect('/contact')
+})
+
 router.get('/cgv', isAuth, async (req, res) => {
 
-    res.render('../views/cgv',  { 
+    res.render('../views/home/cgv',  { 
         user : req.user, 
         page : '/cgv',
         layout: '../views/main' 
@@ -79,6 +171,33 @@ router.post("/create-subscription", async (req, res) => {
                     subscriptionId: subscription.id,
                     /*clientSecret:
                         subscription.latest_invoice?.payment_intent?.client_secret?null,*/
+                });
+
+                  await transporter.sendMail({
+                    from: process.env.SMTP_FROM,
+                    to: rawData.email,
+                    subject: rawData.objet,
+                    html: `Bonjour ${rawData.firstname},
+                            <p>
+                                Nous vous remercions de la confirance que vous nous avez accordé !<br>
+                                Vous trouverez ici le récapitulatif de toute vos données pour vous authetifier
+                            </p>
+                            <p>
+                                <ul>
+                                    <p>Identifiant : ${rawData.email}</p>
+                                    <p>Mot de passe : ${rawData.password}</p>
+                                    Lien de connexion <a href="{domain}/login">ici</a>
+                                </ul>
+                            </p>
+                            <p>
+                                En vous souhaitant le meilleur dans votre progression
+                            </p>
+                            <p>
+                                <a href="regimesenior.fr">regimesenior.fr</a>
+                                <br>
+                                8 Rue Blaja,<br>
+                                31500 Toulous
+                            </p>`
                 });
 
 
